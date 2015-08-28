1fda14a31262b74e9cfbb123966bc6d92a46358c
Eduardo-Amorim <erenkyojin840@gmail.com>
2015-08-28 10:10:33 -0300

    Workspace

---
+++ CardGame/src/Credits.java
@@ -0,0 +1,12 @@
+
+public class Credits {
+
+	public static void main(String[] args) {
+		
+		System.out.println("====Creditos====");
+		System.out.println("MARCOS GOULART");
+		System.out.println("RAIMUNDO LIMA");
+		System.out.println("EDUARDO AMORIM");
+	}
+
+}
