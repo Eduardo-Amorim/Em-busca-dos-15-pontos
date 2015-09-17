9b9aaf6f997296935086748fbae3aeb8ae031daa
Eduardo Amorim <erenkyojin840@gmail.com>
2015-09-16 22:27:43 -0300

    Update Credits.java

--- CardGame/src/Credits.java
+++ CardGame/src/Credits.java
@@ -1,12 +1,27 @@
 
 public class Credits {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		System.out.println("====Creditos====");
-		System.out.println("MARCOS GOULART");
-		System.out.println("RAIMUNDO LIMA");
-		System.out.println("EDUARDO AMORIM");
+		Type("====Creditos====");
+		System.out.println();
+		Type("MARCOS GOULART");
+		System.out.println();
+		Type("RAIMUNDO LIMA");
+		System.out.println();
+		Type("EDUARDO AMORIM");
+		System.out.println();
+		Type("MARCIO TORRES");
+		System.out.println();
+		Type("");
+	}
+
+	public static void Type(String text) throws InterruptedException{
+		char[] textC = text.toCharArray();
+		for(int i = 0; i < textC.length; i++){
+			System.out.print(textC[i]);
+			Thread.sleep(100);
+		}
 	}
 
 }
