9fbd0c1bb01eeaaa0d36c4437a4f33fbdec7742a
Eduardo Amorim <erenkyojin840@gmail.com>
2015-12-06 22:50:45 -0200

    /// Feito: Marcos Goulart

--- CardGameUnity/Assets/Scripts/Pergunta.cs
+++ CardGameUnity/Assets/Scripts/Pergunta.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Pergunta {
 
-	public string Tema { get; set; }
+    public string Tema { get; set; }
     public Texture2D Imagem { get; set; }
     public string Texto { get; set; }
     public string[] Respostas { get; set; }
